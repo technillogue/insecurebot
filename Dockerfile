dea56cdbf06c5351617d207dc7bc8af13ce7ca62
technillogue <technillogue@gmail.com>
2022-03-13 21:02:18 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# FROM registry.gitlab.com/packaging/signal-cli/signal-cli-native:v0-10-4-1 as signal
+# RUN signal-cli --version | tee /signal-version
+# RUN mv /usr/bin/signal-cli-native /usr/bin/signal-cli
+
+FROM ghcr.io/rust-lang/rust:nightly as builder
+WORKDIR /app
+RUN git clone https://github.com/mobilecoinofficial/auxin && cd auxin && git checkout 0.1.13
+WORKDIR /app/auxin
+RUN rustup default nightly
+RUN cargo +nightly build --release
+
+FROM python:3.9 as libbuilder
+WORKDIR /app
+RUN pip install poetry
+RUN python3.9 -m venv /app/venv 
+COPY ./pyproject.toml ./poetry.lock /app/
+RUN VIRTUAL_ENV=/app/venv poetry install 
+
+FROM ubuntu:hirsute
+WORKDIR /app
+RUN mkdir -p /app/data
+RUN apt-get update
+RUN apt-get install -y python3.9 libfuse2
+RUN apt-get clean autoclean && apt-get autoremove --yes && rm -rf /var/lib/{apt,dpkg,cache,log}/
+# COPY --from=signal /usr/bin/signal-cli /signal-version /app/
+# # for signal-cli's unpacking of native deps
+# COPY --from=signal /lib/x86_64-linux-gnu/libz.so.1 /lib64/
+COPY --from=builder /app/auxin/target/release/auxin-cli /app/auxin-cli
+COPY --from=libbuilder /app/venv/lib/python3.9/site-packages /app/
+COPY .git/COMMIT_EDITMSG bot.py /app/ 
+ENTRYPOINT ["/usr/bin/python3.9", "/app/bot.py"]
---
+++ bot.py
@@ -0,0 +1,70 @@
+import asyncio
+from forest import core
+from forest.core import Bot, Message, Response, run_bot, rpc
+
+
+def is_admin(_: Message) -> bool:
+    return True
+
+
+core.is_admin = is_admin
+
+
+class InsecureBot(Bot):
+    async def do_eval(self, msg: Message) -> Response:
+        """Evaluates a few lines of Python. Preface with "return" to reply with result."""
+
+        async def async_exec(stmts: str, env: Optional[dict] = None) -> Any:
+            parsed_stmts = ast.parse(stmts)
+            fn_name = "_async_exec_f"
+            my_fn = f"async def {fn_name}(): pass"
+            parsed_fn = ast.parse(my_fn)
+            for node in parsed_stmts.body:
+                ast.increment_lineno(node)
+            assert isinstance(parsed_fn.body[0], ast.AsyncFunctionDef)
+            # replace the empty async def _async_exec_f(): pass body
+            # with the AST parsed from the message
+            parsed_fn.body[0].body = parsed_stmts.body
+            code = compile(parsed_fn, filename="<ast>", mode="exec")
+            exec(code, env or globals())  # pylint: disable=exec-used
+            # pylint: disable=eval-used
+            try:
+                return await eval(f"{fn_name}()", env or locals())
+            except:  # pylint: disable=bare-except
+                return "".join(traceback.format_exception(*sys.exc_info()))
+
+        if msg.full_text and len(msg.tokens) > 1:
+            source_blob = msg.full_text.replace(msg.arg0, "", 1).lstrip("/ ")
+            env = globals()
+            env.update(locals())
+
+            return str(await async_exec(source_blob, env))
+        return None
+
+    async def do_sh(self, msg: Message) -> str:
+        return "\n".join(
+            map(
+                bytes.decode,
+                filter(
+                    lambda x: isinstance(x, bytes),
+                    await (
+                        await asyncio.create_subprocess_shell(
+                            msg.text, stdout=-1, stderr=-1
+                        )
+                    ).communicate(),
+                ),
+            )
+        )
+
+    async def handle_message(self, message: Message) -> Response:
+        if message.typing == "STARTED":
+            await self.outbox.put(rpc("sendTyping", recipient=[message.source]))
+        if message.typing == "STOPPED":
+            await self.outbox.put(
+                rpc("sendTyping", recipient=[message.source], stop=True)
+            )
+        return await super().handle_message(message)
+
+
+if __name__ == "__main__":
+    run_bot(InsecureBot)
